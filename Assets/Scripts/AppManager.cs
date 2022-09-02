71fd02ed8bc84f756e1096dcad1d981be0f3492f
Ravi Bechoe <35457924+rbechoe@users.noreply.github.com>
2022-09-02 21:16:11 +0200

    initial cloud commit

---
+++ Assets/Scripts/AppManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AppManager : MonoBehaviour
+{
+    public Cities activeCity;
+    public Text roomInfo;
+
+    public string[] CityNames = new string[]
+    {
+        "Utrecht",
+        "Amsterdam",
+        "Den Haag",
+        "Zwolle",
+        "Rotterdam",
+        "Gouda",
+        "Antwerpen",
+        "Essen"
+    };
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void EnterWebsite()
+    {
+        Application.OpenURL("https://www.google.com");
+    }
+
+    public void SetRoom(int cityId)
+    {
+        activeCity = (Cities) cityId;
+        roomInfo.text = "Naar " + CityNames[cityId] + ": " + Random.Range(1, 100) + " buddies";
+    }
+}
+
+public enum Cities
+{
+    Utrecht,
+    Amsterdam,
+    DenHaag,
+    Zwolle,
+    Rotterdam,
+    Gouda,
+    Antwerpen,
+    Essen
+}
